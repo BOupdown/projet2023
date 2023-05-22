f4de457ecff350892212ea7cd8f836aafebf90b6
BOupdown <75842770+BOupdown@users.noreply.github.com>
2023-05-22 10:54:44 +0200

    background

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Page avec image d arrière-plan</title>
+  <style>
+    body {
+      background-image: url(images/2591631.ai.svg);
+      background-repeat: no-repeat;
+      background-size: cover;
+    }
+  </style>
+</head>
+<body>
+  <!-- Le reste de votre contenu HTML -->
+</body>
+</html>
