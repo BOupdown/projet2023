0cc802670c6a42b8a6a2aa6900c7cd3f49d8a5ef
Julien1000 <papinijuli@cy-tech.fr>
2023-05-26 13:18:45 +0200

    index mis à jour et css gagnants

--- index.php
+++ index.php
@@ -1,25 +1,33 @@
 <!DOCTYPE html>
 <html>
+
 <head>
   <title>index IA Pau</title>
   <link rel="stylesheet" type="text/css" href="/css/index.css">
 </head>
 
 <body>
- 
-<?php
-session_start();
-require 'php/navbar.php';
-?>
+
+  <?php
+  session_start();
+  require 'php/navbar.php';
+  ?>
   <div id="DataDefiTitle" class="center-text">Data Défi</div>
 
   <div class="buttonsAccueil">
-    <a href="php/register.php" class="button">S'inscrire</a>
+    <?php
+    if (isset($_SESSION['id'])) {
+      echo '<a href="php/monProfil.php" class="button">Mon profil</a>';
+    } else {
+      echo '    <a href="php/register.php" class="button">S\'inscrire</a>';
+    }
+    ?>
     <a href="php/datainfo.php" class="button">Data infos</a>
     <br>
     <a href="php/inscriptionAdmin.php" class="button">Les gagnants des concours</a>
 
   </div>
-  
+
 </body>
+
 </html>
