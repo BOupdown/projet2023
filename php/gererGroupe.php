211b6970f99e9790f492a0e3296920ab8ca4127e
Couscouz_ <61880638+Couscouz@users.noreply.github.com>
2023-05-30 11:33:18 +0200

    Update gererGroupe.php final

--- php/gererGroupe.php
+++ php/gererGroupe.php
@@ -9,8 +9,7 @@
 
 <body>
     <?php
-    session_start();
-8
+    session_start();    
     /*
     if (empty($_SESSION['type']) ||$_SESSION['type'] != 'Etudiant') {
         header('Location: /index.php');
@@ -25,6 +24,7 @@
     //idGroupe doit etre le name de l'input
     //$idGroupe = $_POST['idGroupe'];
     $idGroupe = 1;
+    $ajouterUser = "toto";
     $connexion = connect($usernamedb, $passworddb, $dbname);
     $groupe = getGroupeParId($connexion, $idGroupe);
     $nomsMembresGroupe = array();
@@ -38,57 +38,115 @@
             $nomsMembresGroupe[] = $user;
         }
     }
-
     disconnect($connexion);
-    //supprimer les gens
-    //supprimer le groupe
-    //ajouter les gens
-    
+
     ?>
     <div class="body">
         <div class="container">
             <div class="title">Gérer <?php echo $groupe["nom"]?></div>
             <div class="content">
+
                 <form id="form" action="process-gerergroupe.php" method="post" target="_self">
                     <input type="hidden" name="idGroupe" value=<?php echo $idGroupe; ?>>
-                    <input type="hidden" name="ajouterUser" value=<?php echo $ajouterUser; ?>>
-                    <div class="user-details">
-                        <div class="input-box">
-                            <span class="details">Supprimer le groupe</span>
-                            <input name="supprimerGroupe" type="checkbox" placeholder="Entrez votre nom" value="supprimer ">
-                        </div>
-
-                        <div class="input-box">
-                            <span class="details">Ajouter un membre</span>
-                            <input name="ajouterUser" type="text" placeholder="Entrez le login" value=<?php if (isset($_SESSION['loginAjouter'])) { echo $_SESSION['loginAjouter'];} ?> >
-                        </div>
-                        <?php
-                        unset($_SESSION['loginAjouter']);
-                        ?>
-                        
-                    <div class="retirer-user">
-                    <span class="niveau-title">Retirer des utilisateurs</span>
-                        <div class="category">
-                        <?php
-                        $i = 1;
-                        foreach($nomsMembresGroupe as $membre)
-                        {
-                            echo "<input name = \"cb\" value=\"".$membre["nomUtilisateur"]."\" type=\"checkbox\" name=\"retirerMembre".$i."\" id=\"check-\"".$i."\">
-                            <label for=\"check-\"".$i."\">".$membre["nomUtilisateur"]."</label>";
-                            $i++;
-                        }
-                        ?>
-                        </div>
-                    </div>
-                    <?php
-                    unset($_SESSION['aRetirerUser']);
-                    ?>
+                    <input type="hidden" name="typeModif" value="supprimer">
                     <div class="button">
-                        <input type="submit" value="Valider">
+                        <input type="submit" value="Supprimer le groupe">
                     </div>
                 </form>
+                <hr>
+                <form id="form" action="process-gerergroupe.php" method="post" target="_self">
+                    <input type="hidden" name="idGroupe" value=<?php echo $idGroupe; ?>>
+                    <input type="hidden" name="typeModif" value="ajouter">
+                    <input type="hidden" name="aAjouter" id="aAjouterUsers" value="">
+                    <div class="user-details">
+                        <div class="input-box">
+                            <span class="details">Ajouter un membre</span>
+                            <div class="input-group">
+                            <input type="text" name="search" id="search" placeholder="Search..." autocomplete="off">
+                            </div>
+                        
+                            <div class="list-group" id="show-list"></div>
+                            <hr>
+                            <div class="list-group" id="ajouterUsers"><p>Seront ajoutés :</p></div>
+                        </div>
+                    <div>
+                    <div class="button">
+                        <input type="submit" value="Ajouter ces users ">
+                    </div>
+                </form>
+                <hr>    
+                <form id="form" action="process-gerergroupe.php" method="post" target="_self">
+                    <input type="hidden" name="idGroupe" value=<?php echo $idGroupe; ?>>
+                    <input type="hidden" name="typeModif" value="retirer">
+                    
+                    <div class="user-details">
+                        <div class="retirer-user input-box">
+                        <span class="niveau-title">Retirer des utilisateurs</span>
+                            <div class="category">
+                            <?php
+                            $i = 1;
+                            foreach($nomsMembresGroupe as $membre)
+                            {
+                                echo "<div><input value=\"".$membre["nomUtilisateur"]."\" type=\"checkbox\" name=\"retirerMembre".$i."\" id=\"check-\"".$i."\">
+                                <label for=\"check-\"".$i."\">".$membre["nomUtilisateur"]."</label></div>";
+                                $i++;
+                            }
+                            ?>
+                            </div>
+                        </div>
+                    <div>
+                    <div class="button">
+                        <input type="submit" value="Retirer les users séléctionnés">
+                    </div>
+                </form>
+                <!-- input type="hidden" name="listeAjoutUser" value=<?php echo $ajouterUser; ?> -->
+                 
             </div>
         </div>
     </div>
 </body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+    var selectedUsers = [];
+
+    $(document).ready(function () {
+        // Send Search Text to the server
+        // Send Search Text to the server
+        $("#search").keyup(function () {
+            let searchText = $(this).val();
+            if (searchText != "") {
+                $.ajax({
+                    url: "recupererEtudiantPasDansGroupe.php",
+                    method: "post",
+                    data: {
+                        query: searchText,
+                        users: selectedUsers,
+                    },
+                    success: function (response) {
+                        $("#show-list").html(response); // Utiliser l'ID dynamique dans le sélecteur
+                    },
+                });
+            } else {
+                $("#show-list").html("");
+            }
+        });
+
+        // Set searched text in input field on click of search button
+        $(document).on("click", "p", function () {
+            var selectedUser = $(this).text();
+            /*
+            var inputHTML = '<input type="checkbox" name="retirerMembre' + (selectedUsers.length + 1) + '" id="check-' + (selectedUsers.length + 1) + '" value="' + selectedUser + '">';
+            var labelHTML = '<label for="check-' + (selectedUsers.length + 1) + '">' + selectedUser + '</label>';
+            */
+            var selectedUserHTML = '<div><p>' + selectedUser + '</p></div>';
+            var ancienneValeur = $("#aAjouterUsers").val();
+            $("#aAjouterUsers").val(ancienneValeur+"-"+selectedUser);
+            $("#ajouterUsers").append(selectedUserHTML);
+            selectedUsers.push(selectedUser);
+            $("#search").val(""); // Utiliser l'ID dynamique dans le sélecteur de l'input
+            $("#show-list").html("");
+        });
+
+    });
+</script>
 </html>
