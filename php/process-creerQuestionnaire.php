e36a161fe11db0287be42a154ef03e596fbf7908
bakura84 <p.theuret@hotmail.com>
2023-05-30 19:48:15 +0200

    creation question oubli

--- php/process-creerQuestionnaire.php
+++ php/process-creerQuestionnaire.php
@@ -1,55 +1,50 @@
 <?php
 
     session_start();
-
-    // Le code ne s'éxécute que s'il existe au moins une question est tapée dans le questionnaire
-    if ($_POST['compteur'] > 0) {
-
-        session_start();
-        
-        // Vérifier que l'utilisateur connecté est gestionnaire
-        if (!isset($_SESSION['type']) || $_SESSION['type'] != 'Gestionnaire') {
-            die("Erreur : Permissions insuffisantes.");
-        }
-
-        require 'fonctionCreateBDD.php';
-
-        $id_sujet = $_POST['projetdata'];
-        $nom_questionnaire = $_POST['nom'];
-        $description_questionnaire = $_POST['description'];
-        $nb_questions = $_POST['compteur'];
-
-        $connexion = connect($usernamedb, $passworddb, $dbname);
-
-        // Création du questionnaire vide dans la base SQL
-        $requete_nouveau_questionnaire = "INSERT INTO Questionnaire (idSujet, nom, descriptionQ) VALUES (?, ?, ?)";
-        $stmt = mysqli_prepare($connexion, $requete_nouveau_questionnaire);
-        mysqli_stmt_bind_param($stmt, "iss", $id_sujet, $nom_questionnaire, $description_questionnaire);
-        mysqli_stmt_execute($stmt);
-
-        // Valeur du dernier ID inséré dans la base SQL (ID du questionnaire)
-        $id_questionnaire = mysqli_insert_id($connexion);
-
-        for ($i = 1; $i <= $nb_questions; $i++) {
-            
-
-
-            $nameHTML_question = 'question-' . $i;
-
-            echo $nameHTML_question;
-
-            // Ajout de la question au questionnaire dans la base SQL
-            $requete_nouvelle_question = "INSERT INTO Question (idQuestionnaire, question) VALUES (?, ?)";
-            $stmt = mysqli_prepare($connexion, $requete_nouvelle_question);
-            mysqli_stmt_bind_param($stmt, "is", $id_questionnaire, $_POST[$nameHTML_question]);
-            mysqli_stmt_execute($stmt);
-          
-        }
-        
-        disconnect($connexion);
-
-        // Redirection avec notification de confirmation
-        $_SESSION['questionnaire-cree'] = 1;
-        header("Location: creerQuestionnaire.php");
+    
+    // Vérifier que l'utilisateur connecté est gestionnaire
+    if (!isset($_SESSION['type']) || $_SESSION['type'] != 'Gestionnaire') {
+        die("Erreur : Permissions insuffisantes.");
     }
+
+     require 'fonctionCreateBDD.php';
+
+     $id_sujet = $_POST['projetdata'];
+    $nom_questionnaire = $_POST['nom'];
+    $description_questionnaire = $_POST['description'];
+    $nb_questions = $_POST['compteur'];
+
+     $connexion = connect($usernamedb, $passworddb, $dbname);
+
+     // Création du questionnaire vide dans la base SQL
+    $requete_nouveau_questionnaire = "INSERT INTO Questionnaire (idSujet, nom, descriptionQ) VALUES (?, ?, ?)";
+    $stmt = mysqli_prepare($connexion, $requete_nouveau_questionnaire);
+    mysqli_stmt_bind_param($stmt, "iss", $id_sujet, $nom_questionnaire, $description_questionnaire);
+    mysqli_stmt_execute($stmt);
+
+     // Valeur du dernier ID inséré dans la base SQL (ID du questionnaire)
+    $id_questionnaire = mysqli_insert_id($connexion);
+
+    for ($i = 1; $i <= $nb_questions; $i++) {
+        
+
+
+        $nameHTML_question = 'question-' . $i;
+
+        echo $nameHTML_question;
+
+        // Ajout de la question au questionnaire dans la base SQL
+        $requete_nouvelle_question = "INSERT INTO Question (idQuestionnaire, question) VALUES (?, ?)";
+        $stmt = mysqli_prepare($connexion, $requete_nouvelle_question);
+        mysqli_stmt_bind_param($stmt, "is", $id_questionnaire, $_POST[$nameHTML_question]);
+        mysqli_stmt_execute($stmt);
+      
+    }
+    
+    disconnect($connexion);
+
+    // Redirection avec notification de confirmation
+    $_SESSION['questionnaire-cree'] = 1;
+    header("Location: creerQuestionnaire.php");
+
 ?>
