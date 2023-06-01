6aec9ec8c9befdbd8a1809572bc7b13bc05da4c3
Julien1000 <papinijuli@cy-tech.fr>
2023-06-01 11:01:04 +0200

    rendu

--- php/monRendu.php
+++ php/monRendu.php
@@ -10,107 +10,94 @@
 </head>
 
 <body>
-<?php
+    <?php
     session_start();
     require 'navbar.php';
     require_once 'fonctionGetBDD.php';
     require_once 'fonctionCreateBDD.php';
 
 
-    if (empty($_SESSION['type']) || $_SESSION['type'] != 'Etudiant' || empty($_GET["idGroupe"]))
-    {
+    if (empty($_SESSION['type']) || $_SESSION['type'] != 'Etudiant' || empty($_GET["idGroupe"])) {
         header('Location: /index.php');
         exit();
     }
-    
+
     $idGroupe = $_GET["idGroupe"];
 
-    if (isset($_FILES["fileUpload"]))
-    {
+    if (isset($_FILES["fileUpload"])) {
         $first = true;
         //on recupere le chemin du fichier temporaire et son nom
         $chemin = $_FILES["fileUpload"]["tmp_name"];
         $nomFichier = $_FILES["fileUpload"]["name"];
-        
+
 
         //recherche d'occurence
-        if (!empty($_POST["aChercher"]))
-        {
+        if (!empty($_POST["aChercher"])) {
             $expressions = explode(",", $_POST["aChercher"]);
             $resultatOccurences = array();
 
             $contenu = file_get_contents($chemin);
 
-            foreach($expressions as $mot)
-            {
+            foreach ($expressions as $mot) {
                 $pattern = preg_quote($mot, '/');
                 $pattern = "/^.*$pattern.*\$/m";
-                
-                if (preg_match_all($pattern, $contenu, $trouves))
-                {
-                    $nb = sizeof($trouves[0]);      
-                }
-                else
-                {
+
+                if (preg_match_all($pattern, $contenu, $trouves)) {
+                    $nb = sizeof($trouves[0]);
+                } else {
                     $nb = 0;
                 }
                 array_push($resultatOccurences, array($mot, $nb));
             }
-        }   
+        }
 
-        $resultat = file_get_contents("http://localhost:8001/analyse-api?param=".$chemin);
-        if (!$resultat)
-        {
+        $resultat = file_get_contents("http://localhost:8001/analyse-api?param=" . $chemin);
+        if (!$resultat) {
             echo "<script>alert('Erreur lors du chargement du fichier')</script>";
             header('Location: /index.php');
         }
         //on appelle l'api avec le fichier
         $json = json_decode($resultat);
 
-        $data = array($json->{"nbLignes"},
+        $data = array(
+            $json->{"nbLignes"},
             $json->{"nbFonctions"},
             $json->{"tailleMinFonction"},
             $json->{"tailleMaxFonction"},
             $json->{"tailleMoyenneFonction"},
         );
-        var_dump($data);
         //ajout dans la bdd
-
+    
         $connexion = connect($usernamedb, $passworddb, $dbname);
         $fichier = getDataFichierParIdGroupe($connexion, $idGroupe);
         disconnect($connexion);
-        var_dump($fichier);
         //on check quand meme que le fichier n'existe pas
-        if (!$fichier["nomFichier"])
-        {
+        if (!$fichier["nomFichier"]) {
             $connexion = connect($usernamedb, $passworddb, $dbname);
             creerDataFichier($connexion, $idGroupe, $idProjetData, $nomFichier, $data[0], $data[1], $data[2], $data[3], intval($data[4]));
             disconnect($connexion);
         }
 
-        
+
 
         $calcul = (100 * $json->{"tailleMoyenneFonction"} * $json->{"nbFonctions"}) / $json->{"nbLignes"};
         $pourcentage = round($calcul);
-    }
-    else
-    {
+    } else {
         $first = false;
 
         $connexion = connect($usernamedb, $passworddb, $dbname);
         $fichier = getDataFichierParIdGroupe($connexion, $idGroupe);
         disconnect($connexion);
-        var_dump($fichier);
-        if (!$fichier)
-        {
+        if (!$fichier) {
             echo "<script>alert('Erreur lors du chargement du fichier')</script>";
             header('Location: /index.php');
         }
-        
+
 
         $nomFichier = $fichier["nomFichier"];
-        
-        $data = array($fichier["nbLignes"],
+
+        $data = array(
+            $fichier["nbLignes"],
             $fichier["nbFonctions"],
             $fichier["tailleMinFonction"],
             $fichier["tailleMaxFonction"],
@@ -119,28 +106,39 @@
         $calcul = (100 * $fichier["tailleMoyenneFonction"] * $fichier["nbFonctions"]) / $fichier["nbLignes"];
         $pourcentage = round($calcul);
     }
-    
-?>
-<div class="body">
-    <div class="container">
-        <div class="content">
-            <div>
-                <canvas id="diagramme"></canvas>
-            </div>
-            <div>
-                <canvas id="diagRadar"></canvas>
-            </div>
-            <div>
-                <canvas id="diagFromage"></canvas>
-            </div>
-            <div>
-                <canvas id="diagBarre"></canvas>
-            </div>
-            <?php
-                if ($first && !empty($_POST["aChercher"]))
-                {
-                echo "<div>
-                        <table>
+
+    ?>
+    <h1 class="titre">Mes rendus</h1>
+
+    <div class="body">
+        <div class="container2">
+            <div class="content">
+                <div class="graph-container">
+                    <div class="graph-pair">
+                        <div class="graph">
+                            <canvas id="diagramme"></canvas>
+                        </div>
+                    </div>
+                    <div class="graph-pair">
+                    <div class="graph">
+                        <canvas id="diagRadar"></canvas>
+                    </div>
+                    </div>
+                    <div class="graph-pair">
+                    <div class="graph">
+                        <canvas id="diagFromage"></canvas>
+                    </div>
+                    </div>
+                    <div class="graph-pair">
+                    <div class="graph">
+                        <canvas id="diagBarre"></canvas>
+                    </div>
+                    </div>
+                </div>
+                <?php
+                if ($first && !empty($_POST["aChercher"])) {
+                    echo "<div>
+                        <table class='table'>
                             <thead> 
                                 <tr>
                                     <th>Mot</th>
@@ -148,13 +146,13 @@
                                 </tr>
                             </thead>
                             <tbody>";
-                                // Parcourir le tableau et générer les lignes HTML correspondantes
-                                foreach ($resultatOccurences as $ligne) {
-                                    $mot = $ligne[0];
-                                    $occurrence = $ligne[1];
-                                    echo "<tr><td>".$mot."</td><td>".$occurrence."</td></tr>";
-                                }
-                            echo "
+                    // Parcourir le tableau et générer les lignes HTML correspondantes
+                    foreach ($resultatOccurences as $ligne) {
+                        $mot = $ligne[0];
+                        $occurrence = $ligne[1];
+                        echo "<tr><td>" . $mot . "</td><td>" . $occurrence . "</td></tr>";
+                    }
+                    echo "
                         </tbody>
                     </table>";
                 }
@@ -162,29 +160,29 @@
             </div>
         </div>
     </div>
-</div>
-</body>    
+    </div>
+</body>
 <script>
     const labels = [
-        'nbLignes', 
-        'nbFonctions', 
-        'tailleMin', 
-        'tailleMax', 
-        'tailleMoyenne' 
+        'nbLignes',
+        'nbFonctions',
+        'tailleMin',
+        'tailleMax',
+        'tailleMoyenne'
     ];
 
 
-    
+
     var config = {
         type: 'line',
         data: {
             labels: labels,
-            datasets: 
-            [{
-                label: 'Analyse de <?php echo $nomFichier;?>',
-                data: <?php echo json_encode($data);?>,
-                backgroundColor: 'rgba(0, 119, 204, 0.3)'
-            }]
+            datasets:
+                [{
+                    label: 'Analyse de <?php echo $nomFichier; ?>',
+                    data: <?php echo json_encode($data); ?>,
+                    backgroundColor: 'rgba(0, 119, 204, 0.3)'
+                }]
         }
     };
 
@@ -192,12 +190,12 @@
         type: 'radar',
         data: {
             labels: labels,
-            datasets: 
-            [{
-                label: 'Analyse de <?php echo $nomFichier;?>',
-                data: <?php echo json_encode($data);?>,
-                backgroundColor: 'rgba(0, 119, 204, 0.3)'
-            }]
+            datasets:
+                [{
+                    label: 'Analyse de <?php echo $nomFichier; ?>',
+                    data: <?php echo json_encode($data); ?>,
+                    backgroundColor: 'rgba(0, 119, 204, 0.3)'
+                }]
         },
         options: {
             elements: {
@@ -207,18 +205,19 @@
             }
         },
     };
-    
+
     var configFromage = {
         type: 'pie',
         data: {
             labels: ['% fonctions', '% code'],
-            datasets: 
-            [{
-                label: 'Analyse de <?php echo $nomFichier;?>',
-                data:[ <?php echo $pourcentage;?>, <?php echo (100-$pourcentage);; ?>],
-                backgroundColor: ['rgba(0, 119, 204, 0.3)','rgba(150, 119, 204, 0.3)'],
-                hoverOffset: 4
-            }]
+            datasets:
+                [{
+                    label: 'Analyse de <?php echo $nomFichier; ?>',
+                    data: [<?php echo $pourcentage; ?>, <?php echo (100 - $pourcentage);
+                       ; ?>],
+                    backgroundColor: ['rgba(0, 119, 204, 0.3)', 'rgba(150, 119, 204, 0.3)'],
+                    hoverOffset: 4
+                }]
         }
     };
 
@@ -226,24 +225,24 @@
         type: 'bar',
         data: {
             labels: ['tailleMinFo', 'tailleMaxFo', 'tailleMoyenneFo'],
-            datasets: 
-            [{
-                label: 'Analyse de <?php echo $nomFichier;?>',
-                data:[ <?php echo $data[2];?>, <?php echo $data[3]; ?>, <?php echo $data[4]; ?>],
-                backgroundColor: ['rgba(0, 119, 204, 0.3)','rgba(150, 119, 204, 0.3)'],
-                hoverOffset: 4
-            }]
+            datasets:
+                [{
+                    label: 'Analyse de <?php echo $nomFichier; ?>',
+                    data: [<?php echo $data[2]; ?>, <?php echo $data[3]; ?>, <?php echo $data[4]; ?>],
+                    backgroundColor: ['rgba(0, 119, 204, 0.3)', 'rgba(150, 119, 204, 0.3)'],
+                    hoverOffset: 4
+                }]
         },
         options: {
             scales: {
-            y: {
-                beginAtZero: true
-            }
+                y: {
+                    beginAtZero: true
+                }
             }
         },
     };
-</script>   
-<script>   
+</script>
+<script>
     const diagramme = new Chart(
         document.getElementById('diagramme'),
         config
@@ -253,7 +252,7 @@
         document.getElementById('diagRadar'),
         configRadar
     );
-    
+
     const diagFromage = new Chart(
         document.getElementById('diagFromage'),
         configFromage
@@ -263,6 +262,6 @@
         document.getElementById('diagBarre'),
         configBarre
     );
-</script>   
-</html>
+</script>
 
+</html>
